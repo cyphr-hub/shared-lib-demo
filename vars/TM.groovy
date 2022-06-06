9f6518457c450412ee5d28a9c8ceebf5ac23b752
nikhsehgal <104775600+nikhsehgal@users.noreply.github.com>
2022-06-06 14:42:56 +0530

    Update TM.groovy

--- vars/TM.groovy
+++ vars/TM.groovy
@@ -1,4 +1,4 @@
 def call(String user, String pass, String jobName, String projectID, Boolean bool){
-    String script = libraryResource('../resources/TM.py')
-    sh "python3 -c '${script}' ${user} ${pass} "$jobName" ${projectID} ${bool}"
+    script = libraryResource '../resources/TM.py'
+    echo script
 }
