4676a7fe5760e50bc20ab1a8852637f48ef91f24
nikhsehgal <104775600+nikhsehgal@users.noreply.github.com>
2022-06-06 15:06:41 +0530

    Update TM.groovy

--- vars/TM.groovy
+++ vars/TM.groovy
@@ -1,5 +1,5 @@
 def call(String user, String pass, String jobName, String projectID, Boolean bool){
-    script = libraryResource '../resources/TM.py'
+    String script = libraryResource('../resources/TM.py')
     writeFile file:'TM.py', text:script
     sh('echo "Here"')
     sh "python3 TM.py ${user} ${pass} \"${jobName}\" ${projectID} ${bool}"
