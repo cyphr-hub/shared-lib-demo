0278efe1893b338f5bc5e62b24e6aabc76031881
nikhsehgal <104775600+nikhsehgal@users.noreply.github.com>
2022-06-07 14:06:12 +0530

    Update TM.groovy

--- vars/TM.groovy
+++ vars/TM.groovy
@@ -2,5 +2,9 @@ def call(String user, String pass, String jobName, String projectID, Boolean boo
     String script = libraryResource('../resources/TM.py')
     writeFile file:'TM.py', text:script
     def output = sh returnStdout:true, script: 'python3 TM.py ' + user + ' ' +  pass + ' "' + jobName + '" ' + projectID + ' ' + bool
+    if (fileExist('TM.py')) {
+        new File('TM.py').delete()
+        echo "File Deleted"
+    }
     return output
 }
