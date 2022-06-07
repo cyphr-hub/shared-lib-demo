ddc0ec7d486f8af53776715964350f8a2cbdb2b3
nikhsehgal <104775600+nikhsehgal@users.noreply.github.com>
2022-06-07 14:13:00 +0530

    Update TM.groovy

--- vars/TM.groovy
+++ vars/TM.groovy
@@ -3,9 +3,7 @@ def call(String user, String pass, String jobName, String projectID, Boolean boo
     writeFile file:'TM.py', text:script
     def output = sh returnStdout:true, script: 'python3 TM.py ' + user + ' ' +  pass + ' "' + jobName + '" ' + projectID + ' ' + bool
     File file = new File("TM.py")
-    if (file.exists()) {
-        file.delete()
-        echo "File Deleted"
-    }
+    file.delete()
+    echo "File Deleted"
     return output
 }
